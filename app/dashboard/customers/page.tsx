806adfb9912f5361778a38bee98ca3f644dc1f06
superstahn <superstahn@gmail.com>
2023-12-08 15:17:10 +0100

    initial commit for my nextjs demo build

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function page() {
+  return (
+    <div>
+      <p>Customer's page</p>
+    </div>
+  );
+}
+
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function page() {
+  return (
+    <div>
+      <p>Invoices page</p>
+    </div>
+  );
+}
+
+export default page;
